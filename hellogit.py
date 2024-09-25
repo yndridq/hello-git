8cc7ac2b4120cb458c0dc8a9b4a15d193b7a3854
yndridq <yndrid346h@gmail.com>
2024-09-25 18:21:39 -0300

    Este es mi primer commit

---
+++ hellogit.py
@@ -0,0 +1,19 @@
+# Para instalar git:brew install git
+#para saber que tengo git instalado:git
+#para saber la vercion de git que esta instalada:git --version ó git -v
+#para pedir ayuda:git --help ó git -h
+#paver el listado de los directorios de tu compu:ls
+#Para buscar el nombre donde esta tu carpeta:cd documents
+# para volver atras: cd ..
+# En la ruta donde me encuentro: pwd
+#crear una carpeta: mkdir "Hello Git"
+#para entrar a la carpeta creada: cd Hello\ Git
+#para abrir visual estudio por la terminal: code .
+#para configurar git:git config --global user.name ó email "Yndrid ó yndrid346h@gmail.com" 
+#para crear un fichero desde la consola: touch hellogit.py
+#Para inicializar el contexto de git master: git init
+#para cambiar el nombre de la rama master a main:git branch -m main
+#para ver el estado de nuestro proyecto: git status
+#para guardarlo en git:git add hellogit.py
+#para cambiar los ficheros: git commit -m "Este es mi primer codigo"
+print("Hello git")
